c41c7e6a21469c5839be0e18c3ba7142633e8f3d
Shai Gerbi <ShaiGerbi@gmail.com>
2020-07-03 17:18:02 +0300

    Create ToDoItem component

---
+++ components/ToDoItem.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <v-list-item>
+
+    <v-list-item-content>
+      <v-list-item-title v-text="title"></v-list-item-title>
+    </v-list-item-content>
+
+    <v-list-item-action>
+      <v-chip-group>
+        <v-chip small>{{ completed }}</v-chip>
+      </v-chip-group>
+    </v-list-item-action>
+
+  </v-list-item>
+
+</template>
+
+<script>
+export default {
+  name: 'ToDoItem',
+
+  props: {
+    title: {
+      required: true,
+      type: String,
+    },
+    completed: {
+      required: true,
+      type: Boolean,
+      default: false,
+    },
+  }
+}
+</script>
