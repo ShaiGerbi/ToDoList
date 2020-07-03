36a5ace130a6893b36312e7a7bed34227382b53e
Shai Gerbi <ShaiGerbi@gmail.com>
2020-07-03 17:18:48 +0300

    Create ToDoList component

---
+++ components/ToDoList.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <v-card>
+    <v-list nav>
+
+      <to-do-item v-for="item in items" :key="item.id" :title="item.title" :completed="item.completed" />
+
+    </v-list>
+  </v-card>
+
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+
+  props: {
+    items: {
+      required: true,
+      types: Array,
+    }
+  },
+}
+</script>
