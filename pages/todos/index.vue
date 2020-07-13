8d1fc4a52f934afe3d45ddec4a3b67fa3131a7c3
Shai Gerbi <ShaiGerbi@gmail.com>
2020-07-13 20:59:28 +0300

    Create todo index page

---
+++ pages/todos/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <to-do-list :items="items" />
+</template>
+
+<script>
+export default {
+  name: 'todos-index',
+
+  data() {
+    return {
+      items: [],
+    }
+  },
+
+  created() {
+    this.getAll();
+  },
+
+  methods: {
+    getAll() {
+      this.$axios.get('todos')
+      .then(response => {
+        this.items = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+  },
+}
+</script>
